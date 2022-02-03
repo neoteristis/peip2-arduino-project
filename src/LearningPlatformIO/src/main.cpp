5356c926055ad66f0028325cb688945596de6edb
HAND42 <hugo.durand971@gmail.com>
2022-02-03 15:55:24 +0100

    Plateformio

--- src/LearningPlatformIO/src/main.cpp
+++ src/LearningPlatformIO/src/main.cpp
@@ -1,36 +1,46 @@
-#include <CustomServos.h>
+// #include <CustomServos.h>
 #include <Arduino.h>
+#include <Servo.h>
 
-CustomServos servo01(2, 0, 180);
-CustomServos servo02(3, 0, 180);
-CustomServos servo03(4, 0, 180);
-CustomServos servo04(5, 0, 180);
-CustomServos servo05(6, 0, 180);
-CustomServos servo06(7, 0, 180);
-CustomServos servo07(8, 0, 180);
-CustomServos servo08(9, 0, 180);
-CustomServos servo09(10, 0, 180);
-CustomServos servo10(11, 0, 180);
-CustomServos servo11(12, 0, 180);
-CustomServos servo12(13, 0, 180);
+Servo servo01;
+
+// CustomServos servo01(7, 0, 180);
+// CustomServos servo02(3, 0, 180);
+// CustomServos servo03(4, 0, 180);
+// CustomServos servo04(5, 0, 180);
+// CustomServos servo05(6, 0, 180);
+// CustomServos servo06(7, 0, 180);
+// CustomServos servo07(8, 0, 180);
+// CustomServos servo08(9, 0, 180);
+// CustomServos servo09(10, 0, 180);
+// CustomServos servo10(11, 0, 180);
+// CustomServos servo11(12, 0, 180);
+// CustomServos servo12(13, 0, 180);
 
 void setup()
 {
     Serial.begin(9600);
+    servo01.attach(2);
 }
 
 void loop()
 {
+    Serial.println("yoooooo");
+    // servo01.write_angle(25);
     servo01.write(25);
-    servo02.write(25);
-    servo03.write(134);
-    servo04.write(90);
-    servo05.write(25);
-    servo06.write(25);
-    servo07.write(155);
-    servo08.write(155);
-    servo09.write(46);
-    servo10.write(70);
-    servo11.write(155);
-    servo12.write(155);
+    delay(1000);
+    servo01.write(100);
+    // servo01.write_angle(100);
+    delay(1000);
+    // servo02.write_angle(25);
+    // servo03.write_angle(134);
+    // servo04.write_angle(90);
+    // servo05.write_angle(25);
+    // servo06.write_angle(25);
+    // servo07.write_angle(155);
+    // servo08.write_angle(155);
+    // servo09.write_angle(46);
+    // servo10.write_angle(70);
+    // servo11.write_angle(155);
+    // servo12.write_angle(155);
 }
